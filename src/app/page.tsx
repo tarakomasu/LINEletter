95fd96f9f074c855729e0888c1cac5835699471d
Nao <nao@NaonoMacBook-Air.local>
2025-08-21 14:23:20 +0900

    feat: 🎸 コンソールで赤っしょん

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { useSession, signIn, signOut } from "next-auth/react"
 
 export default function Home() {
   const { data: session } = useSession()
-
+  console.log(session)
   if (session) {
     return (
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
