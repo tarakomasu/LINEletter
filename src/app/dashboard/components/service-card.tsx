656c797db4cc60e15c743c69118c490df6512189
Nao <nao@NaonoMacBook-Air.local>
2025-08-21 18:01:14 +0900

    ダッシュボード改良

--- src/app/dashboard/components/service-card.tsx
+++ src/app/dashboard/components/service-card.tsx
@@ -1,12 +1,13 @@
-'use client'
+"use client";
 
-import Image from "next/image"
-import { useRouter } from "next/navigation"
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export const cardData = [
   {
     title: "LINEletter",
-    description: "LINEletterは、あなたが作った手紙をLINEで送れるサービスです。作った手紙はLINE以外でも送信、保存ができます",
+    description:
+      "LINEletterは、あなたが作った手紙をLINEで送れるサービスです。作った手紙はLINE以外でも送信、保存ができます",
     imageUrl: "https://placehold.co/600x400?text=Card+1",
     path: "/line-letter",
   },
@@ -22,10 +23,12 @@ export const cardData = [
     imageUrl: "https://placehold.co/600x400?text=Card+3",
     path: "/card3",
   },
-]
+];
 
-export function ServiceCard({ card }: { card: typeof cardData[0] }) {
-  const router = useRouter()
+export function ServiceCard(
+  { card,devise }: { card: (typeof cardData)[0],devise: string }
+) {
+  const router = useRouter();
 
   return (
     <div className="bg-white rounded-lg shadow-md overflow-hidden">
@@ -36,7 +39,7 @@ export function ServiceCard({ card }: { card: typeof cardData[0] }) {
         height={400}
         className="w-full h-48 object-cover cursor-pointer"
         onClick={() => {
-          router.push(`${card.path}`)
+          router.push(`${card.path + -devise}`);
         }}
       />
       <div className="p-6">
@@ -44,5 +47,5 @@ export function ServiceCard({ card }: { card: typeof cardData[0] }) {
         <p className="text-gray-700">{card.description}</p>
       </div>
     </div>
-  )
+  );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@
 
 import { useSession } from "next-auth/react"
 import { redirect } from "next/navigation"
+import { isMobile } from "react-device-detect"
 import { Hero } from "./components/hero"
 import { Main } from "./components/main"
 import { ServiceCard, cardData } from "./components/service-card"
@@ -13,13 +14,13 @@ export default function Dashboard() {
       redirect("/")
     },
   })
-
+  const userDevice = isMobile ? "mobile" : "desktop"
   return (
     <div className="min-h-screen bg-gray-100">
       <Hero />
       <Main>
         {cardData.map((card, index) => (
-          <ServiceCard key={index} card={card} />
+          <ServiceCard key={index} card={card} devise={userDevice}/>
         ))}
       </Main>
     </div>
