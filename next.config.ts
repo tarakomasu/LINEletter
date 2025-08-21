50c4585e7306faf6e8312f965582520241ba2690
Nao <nao@NaonoMacBook-Air.local>
2025-08-21 14:40:33 +0900

    feat: 🎸 image設定

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["profile.line-scdn.net"], // LINEプロフィール画像のドメインを追加
+  },
 };
 
 export default nextConfig;
